167cdf5628b6e91780be78ba9bea39c315df549c
Ankur Agarwal <ankur.a.agarwal@gmail.com>
2020-12-28 21:45:17 -0800

    Change canvas size and background

--- src/demos/MyPApplet.java
+++ src/demos/MyPApplet.java
@@ -16,8 +16,8 @@ public class MyPApplet extends PApplet{
 	
 	public void setup() {
 		//Add setup code for MyPApplet
-		size(400,400);				//set canvas size
-		background(255);			//set canvas color
+		size(600,600);				//set canvas size
+		background(255, 100, 0);			//set canvas color
 		stroke(0);				//set pen color
 		img = loadImage("palmTrees.jpg", "jpg");
 	}
@@ -25,7 +25,7 @@ public class MyPApplet extends PApplet{
 	public void draw() {
 		//Add drawing code for MyPApplet
 		img.resize(0, height);			//resize loaded image to full height of canvas
-		image(img, 0, 0);			//display image 
+		image(img, 0, 0);			//display image
 		int[] color = sunColorSec(second());		//calculate color code for sun
 		fill(color[0],color[1],color[2]);	//set sun color
 		ellipse(width/4,height/5,width/4,height/5);	//draw sun
